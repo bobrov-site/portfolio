a9748a11ffa2b6595be54d2c5b239c7bd4313bbc
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-14 17:29:02 +0300

    add footer

---
+++ src/components/Footer-bar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-primary p-4 mt-4 text-center text-light">
+    <p class="m-0">© 2023 Все права защищены.</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
