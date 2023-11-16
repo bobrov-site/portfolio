23fee6f53b563a89bc867e3d7a1ad2f56dea6ff2
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-16 20:38:10 +0300

    add printboom

--- src/views/PrintboomView.vue
+++ src/views/PrintboomView.vue
@@ -1,11 +1,22 @@
 <template>
-    <div>
-
-    </div>
+    <ProjectContent :siteUrl="siteUrl" :title="title" :images="images" :responsibilities="responsibilities" :achivments="achivments"/>
 </template>
 
 <script setup>
+import ProjectContent from '../components/Project-content.vue';
+const achivments = 'Разработал сайт на Wordpress для компании Print-boom. Сайт получился работоспособен. Сайт приносит заявки на покупку кружек и сувенирной продукции.'
+const responsibilities = [
+    'Разработка сайта на Wordpress по ТЗ',
+]
+const images = [
+    '/src/assets/img/printboom/printboom1.jpeg',
+    '/src/assets/img/printboom/printboom2.jpeg',
+    '/src/assets/img/printboom/printboom3.jpeg',
+    '/src/assets/img/printboom/printboom4.jpeg',
+]
 
+const title = 'Печать фотографий на кружках'
+const siteUrl = 'https://print-boom.ru/'
 </script>
 
 <style lang="scss" scoped>
