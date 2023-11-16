15f29e7a493887bcee3ff8dfcb3864600d49993a
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-16 19:32:32 +0300

    add properegovory

--- src/views/ProperegovoryView.vue
+++ src/views/ProperegovoryView.vue
@@ -1,11 +1,23 @@
 <template>
-    <div>
-
-    </div>
+    <ProjectContent :siteUrl="siteUrl" :title="title" :images="images" :responsibilities="responsibilities" :achivments="achivments"/>
 </template>
 
 <script setup>
+import ProjectContent from '../components/Project-content.vue';
+const achivments = 'Сверстал адаптивно сайт по макету из Figma. Сайт быстро загружается и работает на всех устройствах'
+const responsibilities = [
+    'Верстка по макету при помощи HTML/CSS',
+    'Перенос сайт на хостинг',
+    'Настройка и установка jivochat на сайт'
+]
+const images = [
+    '/src/assets/img/properegovory/properegovory1.jpeg',
+    '/src/assets/img/properegovory/properegovory2.jpeg',
+    '/src/assets/img/properegovory/properegovory3.jpeg',
+]
 
+const title = 'Корпоративные тренинги'
+const siteUrl = 'https://pro-peregovory.ru/'
 </script>
 
 <style lang="scss" scoped>
