ad04ad2af39eb32311e4c47740064b63fb8d42ba
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-17 10:12:48 +0300

    add ascinema in project

--- src/components/Project-content.vue
+++ src/components/Project-content.vue
@@ -5,7 +5,12 @@
         <div class="card p-2 mt-4">
           <div class="card-body">
             <h1 class="card-title">Проект: {{ props.title }}</h1>
-            <div id="carouselExample" class="carousel slide mb-4" data-bs-ride="carousel">
+            <div
+              v-if="props.images"
+              id="carouselExample"
+              class="carousel slide mb-4"
+              data-bs-ride="carousel"
+            >
               <div class="carousel-inner">
                 <div
                   v-for="(item, index) in props.images"
@@ -35,8 +40,11 @@
                 <span class="visually-hidden">Next</span>
               </button>
             </div>
-            <h2 class="card-title">Обязанности:</h2>
-            <ul class="list-group mb-2">
+            <div v-if="props.ascinema" v-html="props.ascinema" class="ascinema w-100">
+              
+            </div>
+            <h2 v-if="props.responsibilities" class="card-title">Обязанности:</h2>
+            <ul v-if="props.responsibilities" class="list-group mb-2">
               <li v-for="item in props.responsibilities" :key="item" class="list-group-item">
                 {{ item }}
               </li>
@@ -46,12 +54,18 @@
               {{ props.achivments }}
             </p>
             <p v-if="props.githubUrl" class="card-text">
-              <a :href="props.githubUrl" class="text-link" target="_blank">Ссылка на проект в Github</a>
+              <a :href="props.githubUrl" class="text-link" target="_blank"
+                >Ссылка на проект в Github</a
+              >
             </p>
             <p v-if="props.tuskUrl" class="card-text">
-              <a :href="props.tuskUrl" class="text-link" target="_blank">Ссылка на поставленную задачу</a>
+              <a :href="props.tuskUrl" class="text-link" target="_blank"
+                >Ссылка на поставленную задачу</a
+              >
             </p>
-            <a v-if="props.siteUrl" :href="props.siteUrl" class="btn btn-primary" target="_blank">Посмотреть сайт</a>
+            <a v-if="props.siteUrl" :href="props.siteUrl" class="btn btn-primary" target="_blank"
+              >Посмотреть сайт</a
+            >
           </div>
         </div>
       </div>
@@ -70,8 +84,17 @@ const props = defineProps({
   githubUrl: String,
   siteUrl: String,
   tuskUrl: String,
+  ascinema: String
 })
 </script>
+
+<style lang="scss">
+.ascinema {
+  img {
+    width: 100%;
+  }
+}
+</style>
 
 <style lang="scss" scoped>
 .text-link {
