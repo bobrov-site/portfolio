eb1f4382d86334be5c94c89ee1ec8e5dd6b692c2
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-17 11:15:30 +0300

    add todolist

--- src/views/TodolistView.vue
+++ src/views/TodolistView.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>
-
-    </div>
+    <ProjectContent :images="images" :ascinema="ascinema" :github-url="githubUrl" :title="title" :responsibilities="responsibilities" :achivments="achivments"/>
 </template>
 
 <script setup>
+import ProjectContent from '../components/Project-content.vue';
+const achivments = 'Создал приложение список задач. Приложение загружает данные по fake api, есть пагинация тудушек. Есть поиск по задачам. Сделал адаптивную версию. Сделал валидацию формы регистрации и входа пользователя. Научился работать с firebase, теперь возможно добавлять новых пользователей, а так же взаимодействовать со старыми.'
+const responsibilities = [
+    'Создание функционала для добавления, удаления и редактирования задач',
+    'Создание возможности сделать логин и регистрацию через почту или google, а так же восстановление пароля',
+    'Создание функционала для редактирования профиля',
+    'Написание валидации для формы регистрации и входа пользователя',
+]
+const images = [
+    '/src/assets/img/todolist/todolist1.jpeg',
+    '/src/assets/img/todolist/todolist2.jpeg',
+    '/src/assets/img/todolist/todolist3.jpeg',
+    '/src/assets/img/todolist/todolist4.jpeg',
+]
 
+const title = 'Тудулист'
+const githubUrl = 'https://github.com/bobrov-site/todolistvue'
 </script>
 
 <style lang="scss" scoped>
