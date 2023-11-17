a4a2263b7a826da0e5f658c10133288aac68aef5
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-17 14:30:54 +0300

    update footer

--- src/components/Footer-bar.vue
+++ src/components/Footer-bar.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="bg-primary p-4 mt-4 text-center text-light">
-    <p class="m-0">© 2023 Все права защищены.</p>
+    <p class="m-2">
+      <a class="text-link text-light" href="https://github.com/bobrov-site/portfolio" target="_blank"> Исходный код сайта на GitHub </a>
+    </p>
+    <p class="m-0 fw-lighter">*Инстаграм запрещён в РФ</p>
   </div>
 </template>
 
