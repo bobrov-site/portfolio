9a01fa884ab36c0d72ffce633de38c0256abec87
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-17 09:36:17 +0300

    add academyofmotivation

--- src/views/AcademyofmotivactionView.vue
+++ src/views/AcademyofmotivactionView.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>
-
-    </div>
+    <ProjectContent :siteUrl="siteUrl" :title="title" :images="images" :responsibilities="responsibilities" :achivments="achivments"/>
 </template>
 
 <script setup>
+import ProjectContent from '../components/Project-content.vue';
+const achivments = 'Разработал сайт по дизайну из Figma на Wordpress для корпоративного тренера из США. Создана система для обучения внутри сайта, где учитель может добавлять курсы и проверять задания учеников. Встроена система оплаты stripe для оплаты курсов'
+const responsibilities = [
+    'Разработка сайта по ТЗ',
+    'Верстка по дизайн-макету при помощи HTML/CSS',
+    'Работа с плагином по созданию уроков',
+    'Добавление системы оплаты',
+    'Создание личного кабинета для учеников и учителя',
+]
+const images = [
+    '/src/assets/img/academyofmotivation/academyofmotivation1.jpeg',
+    '/src/assets/img/academyofmotivation/academyofmotivation2.jpeg',
+    '/src/assets/img/academyofmotivation/academyofmotivation3.jpeg',
+]
 
+const title = 'Корпоративный тренер из США'
+const siteUrl = 'https://academyofmotivaction.com/'
 </script>
 
 <style lang="scss" scoped>
