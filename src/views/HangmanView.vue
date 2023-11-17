7be5118f1a3fd8ec5fedb6e8a5f986f2324ac745
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-17 13:31:32 +0300

    add hangman

--- src/views/HangmanView.vue
+++ src/views/HangmanView.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-
-    </div>
+    <ProjectContent :images="images" :github-url="githubUrl" :title="title" :responsibilities="responsibilities" :achivments="achivments"/>
 </template>
 
 <script setup>
+import ProjectContent from '../components/Project-content.vue';
+const achivments = 'Создал игру "виселица". Изучил основы typescript. Разбил игру по компонентам. Воспроизвел все пользовательские сценарии, чтобы не было неожиданных ошибок или действий от пользователя'
 
+const images = [
+    '/src/assets/img/hangman/hangman1.jpeg',
+    '/src/assets/img/hangman/hangman2.jpeg',
+    '/src/assets/img/hangman/hangman3.jpeg',
+]
+
+const title = 'Виселица (hangman)'
+const githubUrl = 'https://github.com/bobrov-site/todolistvue'
 </script>
 
 <style lang="scss" scoped>
